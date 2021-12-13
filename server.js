ce6c3749ffe14f248d2d115286ff961b36a6a0ff
Your Name <graceliu@gracelius-iMac.home>
2021-12-13 15:24:32 -0500

    successfully connected to the database

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 //import express
 const express = require ('express');
+//import mysql2 package
+const mysql=require('mysql2')
 // add port designation and app expression
 const PORT = process.env.PORT || 3001;
 const app = express();
 //express middleware
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-
-app.get('/',(req,res)=>{
-    res.json({
-        message:'Hello World'
-    })
+//connect to database
+const db =mysql.createConnection(
+    {
+        host:'localhost',
+        user:'root',
+        password:'Ox58807246',
+        database:'election'
+    },
+    console.log('Connected to the election database')
+)
+//querying database to test the connection
+db.query(`SELECT * FROM candidates`, (err,rows)=>{
+    console.log(rows)
 })
 //default response for any other request(Not Found)
 app.use((req,res)=>{
